5597396bf5ad5076f0b0d6283ee99b9634a37061
alfredeveloper <alfredeveloper@gmail>
2018-02-03 20:58:04 -0500

    reddit clone project started

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+@section('content')
+<div class="row">
+  <div class="col-xs-12">
+    <h1>Reddit Clone</h1>
+  </div>
+</div>
+<hr>
+<div class="row">
+  <div class="col-xs-12">
+    <h2>Title</h2>
+    <p>Posted 5 hours ago.</p>
+  </div>
+</div>
+<hr>
+<div class="row">
+  <div class="col-xs-12">
+    <h2>Title</h2>
+    <p>Posted 5 hours ago.</p>
+  </div>
+</div>
+<hr>
+<div class="row">
+  <div class="col-xs-12">
+    <h2>Title</h2>
+    <p>Posted 5 hours ago.</p>
+  </div>
+</div>
+<hr>
+<div class="row">
+  <div class="col-xs-12">
+    <h2>Title</h2>
+    <p>Posted 5 hours ago.</p>
+  </div>
+</div>
+@endsection
---
+++ resources/views/post/show.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('content')
+<div class="row">
+  <div class="col-xs-12">
+    <h2>Title</h2>
+    <p>Description</p>
+    <p>Posted 5 hours ago.</p>
+  </div>
+</div>
+@endsection
